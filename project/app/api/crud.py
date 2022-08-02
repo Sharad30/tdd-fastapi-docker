6f50eb31d7b0c3d0e53f8d681fa3575b86288d3e
Sharad Shriyan <sharad30.ss@gmail.com>
2022-08-02 12:00:33 +0530

    adds test summarization script

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -2,13 +2,11 @@ from typing import List, Union
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+from app.summarizer import generate_summary
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
-    summary = TextSummary(
-        url=payload.url,
-        summary="dummy summary",
-    )
+    summary = TextSummary(url=payload.url, summary="")
     await summary.save()
     return summary.id
 
@@ -23,3 +21,18 @@ async def get(id: int) -> Union[dict, None]:
 async def get_all() -> List:
     summaries = await TextSummary.all().values()
     return summaries
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary
+    return None
