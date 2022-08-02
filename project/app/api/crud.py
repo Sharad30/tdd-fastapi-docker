93d83035536df0ae03b89d01c0b277c80d9e80bb
Sharad Shriyan <sharad30.ss@gmail.com>
2022-08-02 12:09:28 +0530

    ignores flake8

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -2,7 +2,6 @@ from typing import List, Union
 
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
-from app.summarizer import generate_summary
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -29,9 +28,7 @@ async def delete(id: int) -> int:
 
 
 async def put(id: int, payload: SummaryPayloadSchema) -> Union[dict, None]:
-    summary = await TextSummary.filter(id=id).update(
-        url=payload.url, summary=payload.summary
-    )
+    summary = await TextSummary.filter(id=id).update(url=payload.url, summary=payload.summary)
     if summary:
         updated_summary = await TextSummary.filter(id=id).first().values()
         return updated_summary
